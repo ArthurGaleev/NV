2163091769f91bb4d4bdcf433eb80a409c694d43
ArthurGaleev <arturiusgal@gmail.com>
2025-12-05 03:49:06 +0300

    add collate func

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -16,10 +16,11 @@ def collate_fn(dataset_items: list[dict]):
 
     result_batch = {}
 
-    # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
+    result_batch["audio"] = torch.vstack(
+        [elem["audio"] for elem in dataset_items]
     )
-    result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
+    result_batch["mel_spectrogram"] = torch.vstack(
+        [elem["mel_spectrogram"] for elem in dataset_items]
+    )
 
     return result_batch
