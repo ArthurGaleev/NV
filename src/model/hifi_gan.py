31e87d8c043088c62a5a53e2813a87b7f41d2525
ArthurGaleev <arturiusgal@gmail.com>
2025-12-08 01:54:11 +0300

    fix shape msimatches by adding padding in mel spec

--- src/model/hifi_gan.py
+++ src/model/hifi_gan.py
@@ -1,27 +1,40 @@
+from typing import Dict, List, Tuple, Union
+
 import torch
-from torch import nn
-from torch.nn import Sequential
 import torch.nn.functional as F
-from typing import Tuple, List, Dict, Union
+from torch import nn
+
 from src.transforms.mel_spectrogram import MelSpectrogram, MelSpectrogramConfig
 
 
 class Resblock1(nn.Module):
-    def __init__(self, channels: int, k_r: int, dilation_r: List[int]):
+    def __init__(self, input_dim: int, k_r: int, dilation_r: List[int]):
         super().__init__()
 
         self.conv_blocks = nn.ModuleList(
             [
                 nn.Sequential(
                     nn.LeakyReLU(),
-                    nn.Conv1d(channels, channels, kernel_size=k_r, dilation=d_r, padding=(d_r*(k_r-1))//2),
+                    nn.Conv1d(
+                        input_dim,
+                        input_dim,
+                        kernel_size=k_r,
+                        dilation=d_r,
+                        padding=(d_r * (k_r - 1)) // 2,
+                    ),
                     nn.LeakyReLU(),
-                    nn.Conv1d(channels, channels, kernel_size=k_r, dilation=1, padding=(k_r-1)//2)
+                    nn.Conv1d(
+                        input_dim,
+                        input_dim,
+                        kernel_size=k_r,
+                        dilation=1,
+                        padding=(k_r - 1) // 2,
+                    ),
                 )
                 for d_r in dilation_r
             ]
         )
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for conv_block in self.conv_blocks:
             x += conv_block(x)
@@ -29,59 +42,57 @@ class Resblock1(nn.Module):
 
 
 class MRF(nn.Module):
-    def __init__(
-            self, 
-            channels: int, 
-            kernel_r: List[int],
-            dilation_r: List[int]
-        ):
+    def __init__(self, input_dim: int, kernel_r: List[int], dilation_r: List[int]):
         super().__init__()
 
         self.res_blocks = nn.ModuleList(
-            [
-                Resblock1(channels, k_r, dilation_r)
-                for k_r in kernel_r
-            ]
+            [Resblock1(input_dim, k_r, dilation_r) for k_r in kernel_r]
         )
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for res_block in self.res_blocks:
-            x += res_block(x)
+            x = res_block(x)
         return x
+
 
 class Generator(nn.Module):
     def __init__(
-            self, 
-            input_dim: int = 80,
-            hidden_u: int = 512,
-            kernel_u: List[int] = [16, 16, 4, 4],
-            kernel_r: List[int] = [3, 7, 11],
-            dilation_r: List[int] = [1, 3, 5]
-        ):
+        self,
+        input_dim: int = 80,
+        hidden_u: int = 512,
+        kernel_u: List[int] = [16, 16, 4, 4],
+        kernel_r: List[int] = [3, 7, 11],
+        dilation_r: List[int] = [1, 3, 5],
+    ):
         super().__init__()
 
         # 7x1 Conv
-        self.conv_start = nn.Conv1d(input_dim, hidden_u, kernel_size=7, stride=1)
-        
+        self.conv_start = nn.Conv1d(
+            input_dim, hidden_u, kernel_size=7, stride=1, padding=3
+        )
+
         self.convt_mrf_blokcs = nn.Sequential(
             *[
                 nn.Sequential(
                     nn.LeakyReLU(),
                     nn.ConvTranspose1d(
-                        in_channels=hidden_u//(2**l), 
-                        out_channels=hidden_u//(2**(l+1)), 
-                        kernel_size=k_u, 
-                        stride=k_u//2
+                        in_channels=hidden_u // (2**l),
+                        out_channels=hidden_u // (2 ** (l + 1)),
+                        kernel_size=k_u,
+                        stride=k_u // 2,
+                        padding=(k_u - (k_u // 2)) // 2,
                     ),
-                    MRF(hidden_u//(2**(l+1)), kernel_r, dilation_r)
+                    MRF(hidden_u // (2 ** (l + 1)), kernel_r, dilation_r),
                 )
                 for l, k_u in enumerate(kernel_u)
             ]
         )
 
         # 7x1 Conv
-        self.conv_end = nn.Conv1d(hidden_u//(2**(len(kernel_u))), 1, kernel_size=7, stride=1)
-    
+        self.conv_end = nn.Conv1d(
+            hidden_u // (2 ** (len(kernel_u))), 1, kernel_size=7, stride=1, padding=3
+        )
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv_start(x)
         x = self.convt_mrf_blokcs(x)
@@ -102,10 +113,10 @@ class SubMPD(nn.Module):
                 norm_f(nn.Conv2d(64, 128, kernel_size=(5, 1), stride=(3, 1))),
                 norm_f(nn.Conv2d(128, 256, kernel_size=(5, 1), stride=(3, 1))),
                 norm_f(nn.Conv2d(256, 512, kernel_size=(5, 1), stride=(3, 1))),
-                norm_f(nn.Conv2d(512, 1024, kernel_size=(5, 1), stride=1))
+                norm_f(nn.Conv2d(512, 1024, kernel_size=(5, 1), stride=1)),
             ]
         )
-        
+
         # 3x1 Conv
         self.conv_end = norm_f(nn.Conv2d(1024, 1, kernel_size=(3, 1), stride=1))
 
@@ -113,9 +124,9 @@ class SubMPD(nn.Module):
         # pad and reshape
         b, c, t = x.shape
         if t % self.period != 0:
-            n_pad = self.period - (t % self.period)
-            t = t + n_pad
-            x = F.pad(x, (0, n_pad), "reflect")
+            pad_amount = self.period - (t % self.period)
+            t = t + pad_amount
+            x = F.pad(x, (0, pad_amount), "constant")
         x = x.view(b, c, t // self.period, self.period)
 
         layer_features = []
@@ -132,14 +143,11 @@ class SubMPD(nn.Module):
 class MPD(nn.Module):
     def __init__(self):
         super().__init__()
-        self.discriminators = nn.ModuleList(
-            [
-                SubMPD(p)
-                for p in [2, 3, 5, 7, 11]
-            ]
-        )
+        self.discriminators = nn.ModuleList([SubMPD(p) for p in [2, 3, 5, 7, 11]])
 
-    def forward(self, x_real: torch.Tensor, x_fake: torch.Tensor) -> Tuple[torch.Tensor, List[torch.Tensor], torch.Tensor, List[torch.Tensor]]:
+    def forward(
+        self, x_real: torch.Tensor, x_fake: torch.Tensor
+    ) -> Tuple[torch.Tensor, List[torch.Tensor], torch.Tensor, List[torch.Tensor]]:
         x_rs = []
         x_fs = []
         layer_features_rs = []
@@ -159,7 +167,11 @@ class MPD(nn.Module):
 class SubMSD(nn.Module):
     def __init__(self, use_spectral_norm: bool = False):
         super().__init__()
-        norm_f = nn.utils.parametrizations.weight_norm if use_spectral_norm == False else nn.utils.parametrizations.spectral_norm
+        norm_f = (
+            nn.utils.parametrizations.weight_norm
+            if use_spectral_norm == False
+            else nn.utils.parametrizations.spectral_norm
+        )
 
         # 15x1 Conv + 41x1 Convs + 5x1 Conv from MelGAN paper https://arxiv.org/abs/1910.06711
         # with increased layers amount and decreased stride as  mentioned in HiFi-GAN paper
@@ -167,17 +179,35 @@ class SubMSD(nn.Module):
         self.convs = nn.ModuleList(
             [
                 norm_f(nn.Conv1d(1, 16, kernel_size=15, stride=1, padding=7)),
-                norm_f(nn.Conv1d(16, 32, kernel_size=41, stride=2, groups=4, padding=20)),
-                norm_f(nn.Conv1d(32, 64, kernel_size=41, stride=2, groups=8, padding=20)),
-                norm_f(nn.Conv1d(64, 128, kernel_size=41, stride=2, groups=16, padding=20)),
-                norm_f(nn.Conv1d(128, 256, kernel_size=41, stride=2, groups=32, padding=20)),
-                norm_f(nn.Conv1d(256, 512, kernel_size=41, stride=2, groups=64, padding=20)),
-                norm_f(nn.Conv1d(512, 1024, kernel_size=41, stride=2, groups=128, padding=20)),
-                norm_f(nn.Conv1d(1024, 1024, kernel_size=41, stride=2, groups=256, padding=20)),
+                norm_f(
+                    nn.Conv1d(16, 32, kernel_size=41, stride=2, groups=4, padding=20)
+                ),
+                norm_f(
+                    nn.Conv1d(32, 64, kernel_size=41, stride=2, groups=8, padding=20)
+                ),
+                norm_f(
+                    nn.Conv1d(64, 128, kernel_size=41, stride=2, groups=16, padding=20)
+                ),
+                norm_f(
+                    nn.Conv1d(128, 256, kernel_size=41, stride=2, groups=32, padding=20)
+                ),
+                norm_f(
+                    nn.Conv1d(256, 512, kernel_size=41, stride=2, groups=64, padding=20)
+                ),
+                norm_f(
+                    nn.Conv1d(
+                        512, 1024, kernel_size=41, stride=2, groups=128, padding=20
+                    )
+                ),
+                norm_f(
+                    nn.Conv1d(
+                        1024, 1024, kernel_size=41, stride=2, groups=256, padding=20
+                    )
+                ),
                 norm_f(nn.Conv1d(1024, 1024, kernel_size=5, stride=1, padding=2)),
             ]
         )
-        
+
         self.conv_end = norm_f(nn.Conv1d(1024, 1, kernel_size=3, stride=1, padding=1))
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, List[torch.Tensor]]:
@@ -206,21 +236,20 @@ class MSD(nn.Module):
 
         # x2, then x4 average pools
         self.avg_pools = nn.ModuleList(
-            [
-                nn.AvgPool1d(4, 2, padding=2),
-                nn.AvgPool1d(4, 2, padding=2)
-            ]
+            [nn.AvgPool1d(4, 2, padding=2), nn.AvgPool1d(4, 2, padding=2)]
         )
 
-    def forward(self, x_real: torch.Tensor, x_fake: torch.Tensor) -> Tuple[torch.Tensor, List[torch.Tensor], torch.Tensor, List[torch.Tensor]]:
+    def forward(
+        self, x_real: torch.Tensor, x_fake: torch.Tensor
+    ) -> Tuple[torch.Tensor, List[torch.Tensor], torch.Tensor, List[torch.Tensor]]:
         x_rs = []
         x_fs = []
         layer_features_rs = []
         layer_features_fs = []
         for i, discriminator in enumerate(self.discriminators):
             if i != 0:
-                x_real = self.avg_pools[i-1](x_real)
-                x_fake = self.avg_pools[i-1](x_fake)
+                x_real = self.avg_pools[i - 1](x_real)
+                x_fake = self.avg_pools[i - 1](x_fake)
 
             x_r, layer_feature_r = discriminator(x_real)
             x_rs.append(x_r)
@@ -239,12 +268,12 @@ class HiFiGAN(nn.Module):
     """
 
     def __init__(
-            self,
-            hidden_u: int = 512,
-            kernel_u: List[int] = [16, 16, 4, 4],
-            kernel_r: List[int] = [3, 7, 11],
-            dilation_r: List[int] = [1, 3, 5]
-        ):
+        self,
+        hidden_u: int = 512,
+        kernel_u: List[int] = [16, 16, 4, 4],
+        kernel_r: List[int] = [3, 7, 11],
+        dilation_r: List[int] = [1, 3, 5],
+    ):
         """
         Args:
             hidden_u (int): hidden size of generator.
@@ -256,11 +285,11 @@ class HiFiGAN(nn.Module):
 
         # Generator
         self.generator = Generator(
-            input_dim=80, 
+            input_dim=80,
             hidden_u=hidden_u,
             kernel_u=kernel_u,
             kernel_r=kernel_r,
-            dilation_r=dilation_r
+            dilation_r=dilation_r,
         )
 
         # Discriminators
@@ -270,39 +299,81 @@ class HiFiGAN(nn.Module):
         self.get_mel_spectrogram = MelSpectrogram(MelSpectrogramConfig)
 
     def forward(
-            self, 
-            audio_real: torch.Tensor, 
-            mel_spectrogram_real: torch.Tensor
-        ) -> Dict[str, Union[torch.Tensor, List[torch.Tensor]]]:
+        self,
+        audio_real: torch.Tensor,
+        mel_spectrogram_real: torch.Tensor,
+        first_stage: bool = None,
+        audio_fake: torch.Tensor = None,
+    ) -> Dict[str, Union[torch.Tensor, List[torch.Tensor]]]:
         """
         Model forward method.
 
         Args:
             audio_real (Tensor): real audio.
             mel_spectrogram_real (Tensor): its real mel spectrogram.
+            first_stage (bool): determines the stage of the training, e.g. None stage (run generator), first stage (fix G, upd D), second stage (upd G).
+            audio_fake (Tensor): fake audio for the first&second stage, need to pass from the None stage.
         Returns:
-            output (dict): output dict containing new generated audio, its mel spectrogram, mpd and msd output with layer features.
+            output (dict): output dict containing new generated(fake) audio, its mel spectrogram, mpd and msd output with layer features (real/fake).
         """
 
-        # Generator
-        audio_fake = self.generator(mel_spectrogram_real)
-        mel_spectrogram_fake = self.get_mel_spectrogram(audio_fake.squeeze(1))
+        # runs Generator
+        if first_stage == None:
+            # Generator
+            audio_fake = self.generator(mel_spectrogram_real).squeeze(1)
+            mel_spectrogram_fake = self.get_mel_spectrogram(audio_fake)
 
-        # Discriminators
+            return {
+                "audio_fake": audio_fake,
+                "mel_spectrogram_fake": mel_spectrogram_fake,
+            }
+        
         audio_real = audio_real.unsqueeze(1)
-        # MPD
-        x_real_mpd, ftrs_real_mpd, x_fake_mpd, ftrs_fake_mpd = self.mpd(audio_real, audio_fake)
+        audio_fake = audio_fake.unsqueeze(1)
+
+        # runs Discriminator with fixed generated(fake) audio
+        if first_stage:
+            # Discriminators
+
+            # MPD
+            x_mpd_real, ftrs_mpd_real, x_mpd_fake, ftrs_mpd_fake = self.mpd(
+                audio_real, audio_fake.detach()
+            )
+
+            # MPD
+            x_msd_real, ftrs_msd_real, x_msd_fake, ftrs_msd_fake = self.msd(
+                audio_real, audio_fake.detach()
+            )
+
+            return {
+                "mpd": x_mpd_real,
+                "mpd_fake": x_mpd_fake,
+                "msd": x_msd_real,
+                "msd_fake": x_msd_fake,
+            }
+
+        # otherwise runs only Discriminator
+        # Discriminators
 
         # MPD
-        x_real_msd, ftrs_real_msd, x_fake_msd, ftrs_fake_msd = self.msd(audio_real, audio_fake)
+        x_mpd_real, ftrs_mpd_real, x_mpd_fake, ftrs_mpd_fake = self.mpd(
+            audio_real, audio_fake
+        )
+
+        # MPD
+        x_msd_real, ftrs_msd_real, x_msd_fake, ftrs_msd_fake = self.msd(
+            audio_real, audio_fake
+        )
 
         return {
-            "audio_new": audio_fake,
-            "mel_spectrogram_new": mel_spectrogram_fake,
-            "mpd": x_real_mpd,
-            "mpd_features": ftrs_real_mpd,
-            "msd": x_real_msd,
-            "msd_features": ftrs_real_msd,
+            "mpd": x_mpd_real,
+            "mpd_features": ftrs_mpd_real,
+            "mpd_fake": x_mpd_fake,
+            "mpd_features_fake": ftrs_mpd_fake,
+            "msd": x_msd_real,
+            "msd_features": ftrs_msd_real,
+            "msd_fake": x_msd_fake,
+            "msd_features_fake": ftrs_msd_fake,
         }
 
     def __str__(self):
--- src/transforms/mel_spectrogram.py
+++ src/transforms/mel_spectrogram.py
@@ -2,10 +2,9 @@ from dataclasses import dataclass
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 import torchaudio
-
-import librosa
 
 
 @dataclass
@@ -17,11 +16,8 @@ class MelSpectrogramConfig:
     f_min: int = 0
     f_max: int = 8000
     n_mels: int = 80
-    power: float = 1.0
 
-    # value of melspectrograms if we fed a silence into `MelSpectrogram`
-    pad_value: float = -11.5129251
-
+    audio_len_for_mel_spec: int = 8960
 
 class MelSpectrogram(nn.Module):
 
@@ -37,22 +33,9 @@ class MelSpectrogram(nn.Module):
             n_fft=config.n_fft,
             f_min=config.f_min,
             f_max=config.f_max,
-            n_mels=config.n_mels
-        )
-
-        # The is no way to set power in constructor in 0.5.0 version.
-        self.mel_spectrogram.spectrogram.power = config.power
-
-        # Default `torchaudio` mel basis uses HTK formula. In order to be compatible with WaveGlow
-        # we decided to use Slaney one instead (as well as `librosa` does by default).
-        mel_basis = librosa.filters.mel(
-            sr=config.sr,
-            n_fft=config.n_fft,
             n_mels=config.n_mels,
-            fmin=config.f_min,
-            fmax=config.f_max
-        ).T
-        self.mel_spectrogram.mel_scale.fb.copy_(torch.tensor(mel_basis))
+            center=False
+        )
 
     def forward(self, audio: torch.Tensor) -> torch.Tensor:
         """
@@ -60,9 +43,12 @@ class MelSpectrogram(nn.Module):
         :return: Shape is [B, n_mels, T']
         """
 
-        mel = self.mel_spectrogram(audio) \
-            .clamp_(min=1e-5) \
-            .log_()
+        # pad to 8960, so mel spec size would be 32
+        if audio.shape[1] != self.config.audio_len_for_mel_spec:
+            pad_amount = self.config.audio_len_for_mel_spec - audio.shape[1]
+            audio = F.pad(audio, (0, pad_amount), mode='constant')
+
+        mel = self.mel_spectrogram.to(audio.device)(audio)
 
         return mel
     
