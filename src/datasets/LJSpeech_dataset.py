97d2dc48199b50bb341308ec7d9dcf65ce1d8ddf
ArthurGaleev <arturiusgal@gmail.com>
2025-12-07 04:28:48 +0300

    add LJSpeech dataset

--- src/datasets/LJSpeech_dataset.py
+++ src/datasets/LJSpeech_dataset.py
@@ -6,6 +6,7 @@ from tqdm.auto import tqdm
 from src.datasets.base_dataset import BaseDataset
 from src.utils.io_utils import ROOT_PATH, read_json, write_json
 import os
+import kaggle
 
 
 class LJSpeechDataset(BaseDataset):
@@ -17,22 +18,35 @@ class LJSpeechDataset(BaseDataset):
     """
 
     def __init__(
-        self, name="train", *args, **kwargs
+        self, audio_len=8192, sample_rate=22050, *args, **kwargs
     ):
         """
         Args:
-            input_length (int): length of the random vector.
-            n_classes (int): number of classes.
-            dataset_length (int): the total number of elements in
-                this random dataset.
             name (str): partition name
         """
-        wavs_dir = ROOT_PATH / "data" / "datasets" / "LJSpeech" / "wavs"
+        path_dir = ROOT_PATH / "data" / "datasets"
+        data_dir = path_dir / "LJSpeech-1.1"
+        audio_dir = data_dir / "wavs"
+        audio_crop_dir = data_dir / f"wavs_crop-{audio_len}"
 
-        assert wavs_dir.exists(), "to use LJSpeech as dataset, place it's wavs into wavs folder in ./data/datasets/LJSpeech"
+        if not data_dir.exists():
+            kaggle.api.dataset_download_files('mathurinache/the-lj-speech-dataset', path=path_dir, quiet=False, unzip=True)
 
         data = []
-        for wav_path in tqdm(list(wavs_dir.iterdir()), desc="Creating dataset"):
-            data.append({"audio_path": str(wav_path)})
+        for audio_path in list(audio_dir.iterdir()):
+            data.append(audio_path)
+
+        if not audio_crop_dir.exists():
+            audio_crop_dir.mkdir(exist_ok=True, parents=True)
+
+            for audio_path in tqdm(data, total=len(data), desc="Making audio crops for the dataset"):
+                audio, sr = torchaudio.load(audio_path)
+                for i, audio_start in enumerate(range(0, audio.shape[1] - audio_len + 1, audio_len)):
+                    audio_crop_path = audio_crop_dir / (audio_path.stem + f"-{i}" + ".wav")
+                    torchaudio.save(audio_crop_path, audio[:, audio_start:audio_start+audio_len], sample_rate)
+
+        data = []
+        for audio_path in tqdm(list(audio_crop_dir.iterdir()), desc="Creating train dataset"):
+            data.append({"audio_path": str(audio_path)})
 
         super().__init__(data, *args, **kwargs)
