fix mos score calculation

src/metrics/mos_score.py
@@ -2,8 +2,6 @@ import torch
 
 from src.metrics.base_metric import BaseMetric
 from src.utils.wvmos import get_wvmos
-import torchaudio
-from src.transforms.mel_spectrogram import MelSpectrogramConfig
 
 
 class MOS(BaseMetric):
@@ -25,6 +23,12 @@ class MOS(BaseMetric):
         Returns:
             metric (float): calculated (mean) mos score.
         """
-        if audio.dim == 3:
-            return self.model.get_mos_score_batch(audio)
-        return self.model.get_mos_score(audio)
+        # convert to batch format if needed
+        if audio.dim() == 1:
+            audio = audio.unsqueeze(0)
+        
+        mos_scores = []
+        for i in range(audio.shape[0]):
+            mos_scores.append(self.model.get_mos_score(audio[i]))
+
+        return torch.tensor(mos_scores).mean().item()