0308c105d07ded72e2965dae7ea061268951067e
ArthurGaleev <arturiusgal@gmail.com>
2025-12-10 04:21:31 +0300

    fix collate + add separate dataset configs

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -18,31 +18,31 @@ def collate_fn(dataset_items: list[dict]):
     result_batch = {}
 
     if "audio" in dataset_items[0].keys():
-        result_batch["audio"] = torch.vstack([elem["audio"] for elem in dataset_items])
-        # result_batch["audio_len"] = [elem["audio"].shape[1] for elem in dataset_items]
-        # max_len = max(result_batch["audio_len"])
-        # result_batch["audio"] = torch.vstack(
-        #     [
-        #         F.pad(elem["audio"], (0, max_len-elem["audio"].shape[1]), mode="replicate") 
-        #         for elem in dataset_items
-        #     ]
-        # )
+        # result_batch["audio"] = torch.vstack([elem["audio"] for elem in dataset_items])
+        result_batch["audio_len"] = [elem["audio"].shape[1] for elem in dataset_items]
+        max_len = max(result_batch["audio_len"])
+        result_batch["audio"] = torch.vstack(
+            [
+                F.pad(elem["audio"], (0, max_len-elem["audio"].shape[1]), mode="replicate") 
+                for elem in dataset_items
+            ]
+        )
     if "audio_path" in dataset_items[0].keys():
         result_batch["audio_path"] = [elem["audio_path"] for elem in dataset_items]
     if "text_path" in dataset_items[0].keys():
         result_batch["text_path"] = [elem["text_path"] for elem in dataset_items]
 
-    result_batch["mel_spectrogram"] = torch.vstack(
-        [elem["mel_spectrogram"] for elem in dataset_items]
-    )
-    # result_batch["mel_spectrogram_len"] = [elem["mel_spectrogram_len"].shape[2] for elem in dataset_items]
-    # max_len = max(result_batch["mel_spectrogram_len"])
     # result_batch["mel_spectrogram"] = torch.vstack(
-    #         [
-    #             F.pad(elem["mel_spectrogram"], (0, max_len-elem["mel_spectrogram"].shape[2]), mode="replicate") 
-    #             for elem in dataset_items
-    #         ]
-    #     )
+    #     [elem["mel_spectrogram"] for elem in dataset_items]
+    # )
+    result_batch["mel_spectrogram_len"] = [elem["mel_spectrogram_len"].shape[2] for elem in dataset_items]
+    max_len = max(result_batch["mel_spectrogram_len"])
+    result_batch["mel_spectrogram"] = torch.vstack(
+            [
+                F.pad(elem["mel_spectrogram"], (0, max_len-elem["mel_spectrogram"].shape[2]), mode="replicate") 
+                for elem in dataset_items
+            ]
+        )
 
 
     return result_batch
